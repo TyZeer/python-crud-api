68421492237df0c341c6cb5f3e91fd2c76cc7361
Tyzeer <d.pavlov04@gmail.com>
2024-06-24 11:27:22 +0300

    Started basic crud with db

---
+++ core/models/__init__.py
@@ -0,0 +1,9 @@
+__all__ = (
+    "Base",
+    "db_helper",
+    "Product",
+)
+
+from .base import Base
+from .product import Product
+from .db_helper import db_helper, DataBaseHelper
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from typing import Annotated
 from fastapi import FastAPI, Body, Path
 from pydantic import EmailStr, BaseModel
+from core.models import Base, db_helper
+
 from items_views import router as items_router
 from users.views import router as user_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 # app.include_router(items_router, prefix="/items-views") Можно так или
 app.include_router(items_router)
+
 app.include_router(user_router)
 
 
