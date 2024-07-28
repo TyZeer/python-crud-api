1883f97f88df494905be45d3157b4d3d3f55046f
TyZeer <d.pavlov04@gmail.com>
2024-07-28 18:29:15 +0300

    Many very many fucking changes

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,6 +1,13 @@
-__all__ = ("Base", "db_helper", "Product", "User")
+__all__ = (
+    "Base",
+    "db_helper",
+    "Product",
+    "User",
+    "Post",
+)
 
 from .base import Base
+from .post import Post
 from .product import Product
 from .db_helper import db_helper, DataBaseHelper
 from .user import User
